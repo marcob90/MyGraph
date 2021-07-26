fa0bfaba528309c26c6cea7906dc2220bda5add2
Marco B. Shafer <64871866+marcob90@users.noreply.github.com>
2021-07-26 07:27:53 +0000

    Update GraphMain.cpp

--- GraphMain.cpp
+++ GraphMain.cpp
@@ -1,3 +1,5 @@
+/* Copyright (C) 2020 Marco B. Shafer. All rights reserved.*/
+
 #include "Graph.h"
 
 bool thereIsRoute(Graph &g, Node &s, Node &e)
@@ -15,26 +17,23 @@ int main()
 	Node n4(4);
 	Node n5(5);
 	
-    // Create the graph 
-    Graph G(false); 
-    G.addEdge(&n0, &n1); 
-    G.addEdge(&n0, &n5); 
-    G.addEdge(&n5, &n4); 
-    G.addEdge(&n2, &n0); 
-    G.addEdge(&n2, &n1); 
-    G.addEdge(&n1, &n3); 
-    //G.addEdge(&n3, &n1); 
-   // G.addEdge(&n4, &n2); 
-    //G.addEdge(&n2, &n1); 
-  
-    // Perform DFS 
-    G.BFS(&n0); 
+	// Create the graph
+	Graph G(false); 
+        G.addEdge(&n0, &n1); 
+        G.addEdge(&n0, &n5); 
+        G.addEdge(&n5, &n4); 
+        G.addEdge(&n2, &n0); 
+        G.addEdge(&n2, &n1); 
+        G.addEdge(&n1, &n3); 
+
+        // Perform DFS 
+        G.BFS(&n0); 
 	cout << endl;
-    G.DFS(&n0);
-    cout << endl;
-    
-    cout << thereIsRoute(G, n0, n2);
-    cout << endl;
-    
+        G.DFS(&n0);
+        cout << endl;
+
+        cout << thereIsRoute(G, n0, n2);
+        cout << endl;
+
    // cout << G.thereIsCycle();
 } 
