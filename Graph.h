7312f4f4eb1e571bf53215708d0b1f74308724e9
Marco B. Shafer <64871866+marcob90@users.noreply.github.com>
2021-07-26 08:17:56 +0000

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,7 +1,7 @@
 /* Copyright (C) 2020 Marco B. Shafer. All rights reserved.*/
 
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <unordered_set> 
+#include <vector>
 
 enum State {
 	UNVISITED,
@@ -24,29 +24,29 @@ class Node {
 		State getState() const{
 			return state;
 		}
-    
+	
 	private:
 		int data;
 		State state;
-		unordered_set<Node*> children;
+		std::unordered_set<Node*> children;
 };
   
 class Graph { 
   
     // Adjacency list
-    unordered_set<Node*> nodes;
+    std::unordered_set<Node*> nodes;
    
     //directed or undirected graph
     bool isDirected;
     
     //util function to get the adj nodes to input node
-    vector<Node*> getAdjNodes(Node*) const;
+    std::vector<Node*> getAdjNodes(Node*) const;
     
     //util function to detect cycles in directed graph
-    bool thereIsCycle(Node*, unordered_set<Node*> &, unordered_set<Node*> &) const;
+    bool thereIsCycle(Node*, std::unordered_set<Node*> &, std::unordered_set<Node*> &) const;
     
     //util function to detect cycles in undirected graph
-    bool thereIsCycle(Node*, unordered_set<Node*> &, Node* = NULL) const;
+    bool thereIsCycle(Node*, std::unordered_set<Node*> &, Node* = NULL) const;
     
     //set the state of each node of the graph to UNVISITED
     void resetStates(){
